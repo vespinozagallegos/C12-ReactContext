9f17faf2283fb52805abb0ed82d502ab580918d7
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-07 22:16:23 -0300

    Finalmente se envuelve la App con TodosProvider para que todos los componentes tengan acceso

--- Clase12ReactContext/src/main.jsx
+++ Clase12ReactContext/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { TodosProvider } from './context/Todos.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TodosProvider>
+      <App />
+    </TodosProvider>
   </StrictMode>,
 )
