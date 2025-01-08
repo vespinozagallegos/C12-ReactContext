2fcb1bb5d8858b0d9525743f9fe481ee00a0bfbf
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-07 21:47:38 -0300

    Se importo el css module

--- Clase12ReactContext/src/App.jsx
+++ Clase12ReactContext/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import styles from './App.module.css'
 
 function App() {
 
 
   return (
     <>
-      
+      <h1>HOLA</h1>
     </>
   )
 }
