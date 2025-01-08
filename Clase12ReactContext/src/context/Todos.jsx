45ed3a3185350451d76292f0fd358a76cf510209
vespinozagallegos <espinozagallegosvictor@gmail.com>
2025-01-07 21:54:37 -0300

    Se crea el CONTEXT

---
+++ Clase12ReactContext/src/context/Todos.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+export const TodosContext = createContext({
+	//propiedad toDos con un [] 
+	todos: [],			//<--- el array se podra extraer con otro Hook
+	});
